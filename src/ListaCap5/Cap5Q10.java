f2fcfab28ac429d80f7784be2a09e92c45676882
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-08 12:00:25 -0300

    Cap5 start

---
+++ src/ListaCap5/Cap5Q10.java
@@ -0,0 +1,76 @@
+package ListaCap5;
+
+public class Cap5Q10 {
+
+    // 5.1 O o que o seguinte programa faz?
+
+    public static void main(String []args) {
+
+        for ( int i = 1; i <= 10 ; i++ ) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print('0');
+            System.out.println();
+            } //fim do for interno
+        } //fim do for externo
+    } // ele vai imprimir '0' umas 50 vezes.
+
+    /*
+    output:
+
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+    0
+
+Process finished with exit code 0
+
+
+     */
+}
---
+++ src/ListaCap5/Cap5Q6.java
@@ -0,0 +1,20 @@
+package ListaCap5;
+
+public class Cap5Q6 {
+
+    /*
+    5.6 Compare e contraste as instrucoes de repeticao 'while' e 'for'.
+     */
+
+    public static void main(String []args) {
+        /*
+        Laco While = executa um bloco de codigo enquanto sua condicao for verdadeira, quando determinada condicao
+        passa a ser lida como Falsa ou 'condicao alcancada', o bloco de instrucoes contido no laco deixara de ser
+        executado - recomenda-se o uso de incremento para finalizar o laco.
+
+        Laco for = age de forma semelhante ao while; com a diferenca que o incremento deve ser adicionado da estrutura
+        do proprio laco. Aqui, nao necessariamente deve haver um 'False', mas sim que a variavel de controle chegue a
+        um determinado valor para finalizar a execucao do bloco de codigo.
+         */
+    }
+}
