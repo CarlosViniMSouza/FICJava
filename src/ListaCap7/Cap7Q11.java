241ca0ae40e10c3da2dcac1199df75073f738c15
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-24 15:24:47 -0300

    Cap7 - Questions add

---
+++ src/ListaCap7/Cap7Q11.java
@@ -0,0 +1,16 @@
+package ListaCap7;
+
+public class Cap7Q11 {
+
+    /*
+    Escreva instrucoes que realizam as seguintes operações de um array multidimensional:
+
+    a) Configure os 10 elementos do array de inteiros 'counts' como zeros.
+    b) Adicione um a cada um dos 15 elementos do array de inteiros bonus.
+    c) Exiba os cinco valores de array de inteiros 'bestScores' em formato de coluna.
+     */
+
+    public static void main(String []args) {
+
+    }
+}
---
+++ src/ListaCap7/Cap7Q9.java
@@ -0,0 +1,28 @@
+package ListaCap7;
+
+public class Cap7Q9 {
+
+    /*
+    Considere um array de inteiros dois por três t.
+
+    a) Escreva uma instrucao que declara e cria t.
+    b) Quantas linha tem t?
+    c) Quantas colunas tem t?
+    d) Quantos elementos tem t?
+    e) Escreva expressoes de acesso para todos os elementos na linha 1 de t.
+    f) Escreva expressoes de acesso para todos os elementos na coluna 2 de t.
+    g) Escreva uma unica instrucao que configura o elemento de t na linha O e na coluna I como zero.
+    h) Escreva instrucoes especificas para inicializar cada elemento de t em zero.
+    i) Escreva uma instrucao 'for' aninhada que inicializa cada elemento de t como zero.
+    j} Escreva uma instruçao 'for' aninhada que insere os valores para os elementos de t a partir do usuario.
+    k) Escreva uma serie de instrucoes que determina e exibe o valor menor em t.
+    I) Escreva uma unica instrucao printf que exibe os elementos da primeira linha de t.
+    m) Escreva uma instrucao que soma os elementos da terceira coluna de t. Não utilize repeticao.
+    n) Escreva uma serie de instrucoes que exibe o conteudo de t no formato tabular. Liste os indices de coluna
+    como titulos na parte superior e liste os indices de linha a esquerda de cada linha.
+     */
+
+    public static void main(String []args) {
+
+    }
+}
