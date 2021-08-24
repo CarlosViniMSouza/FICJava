1912ce5d3650a8d79ecd95dd2f502077d1c11277
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-24 19:32:59 -0300

    Cap6 completed

--- src/ListaCap6/Cap6Q20.java
+++ src/ListaCap6/Cap6Q20.java
@@ -1,5 +1,7 @@
 package ListaCap6;
 
+import java.util.Scanner;
+
 public class Cap6Q20 {
 
     /*
@@ -7,7 +9,25 @@ public class Cap6Q20 {
     para calcular a área do círculo.
      */
 
-    public static void main(String []args) {
+    public static class Result {
 
+        public void Circumference() {
+            Scanner input = new Scanner( System.in );
+
+            System.out.print("Digite o raio do circulo: " );
+            float rain = input.nextFloat();
+
+            float resp = circleArea(rain);
+            System.out.println("A circunferencia mede: " + resp);
+        }
+
+        public float circleArea(float rain) {
+            return (float) (2 * Math.PI * rain);
+        }
+    }
+
+    public static void main(String []args) {
+        Result circle_Area = new Result();
+        circle_Area.Circumference();
     }
 }
