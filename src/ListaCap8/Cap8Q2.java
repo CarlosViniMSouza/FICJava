3529108ec16df6e1dc2ac419b529de2d936212d9
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-25 09:25:14 -0300

    Cap8 - part1 ok

--- src/ListaCap8/Cap8Q2.java
+++ src/ListaCap8/Cap8Q2.java
@@ -7,6 +7,8 @@ public class Cap8Q2 {
      */
 
     public static void main(String []args) {
-
+        // Resp.: Bem, alem da questao de organizacao, temos uma dependencia no projeto (ou seja, existem pacotes
+        // externos que exercem determinadas funcoes no codigo; e se ocorrerem mudancas nas dependencias, a funcionalidade
+        // fica comprometida).
     }
 }
