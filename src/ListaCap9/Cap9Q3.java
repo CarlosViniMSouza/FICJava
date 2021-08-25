1b1f94a411a1d46289e018bcc74c3595814a4e9e
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-08-25 15:21:58 -0300

    Cap9 - 2 answer responded

--- src/ListaCap9/Cap9Q3.java
+++ src/ListaCap9/Cap9Q3.java
@@ -8,7 +8,48 @@ public class Cap9Q3 {
     composicao em vez da heranca.
      */
 
-    public static void main(String []args) {
+    public static class BasePlusCommissionEmployee extends CommissionEmployee {
+        private double baseSalary;
 
+        public BasePlusCommissionEmployee(String first, String last, String ssn,
+                                           double sales, double rate, double salary) {
+            super(first, last, ssn, sales, rate);
+            setBaseSalary( salary );
+        }
+
+        public void setBaseSalary( double salary ) {
+            baseSalary = ( (salary < 0.0) ? 0.0 : salary);
+        }
+
+        public double getBaseSalary() {
+            return baseSalary;
+        }
+
+        @Override //indica que esse método sobrescreve um método de superclasse
+        public double earnings() {
+            return (getBaseSalary() + super.earnings());
+        }
+
+        @Override // indica que esse método sobrescreve um método de superclasse
+        public String toString () {
+            return String.format( "%s %s\n%s: %.2f" , "base-salaried" ,
+                    super.toString(), "base salary" , getBaseSalary() );
+        }
     }
+
+    private static class CommissionEmployee {
+        public CommissionEmployee(String first, String last, String ssn, double sales, double rate) {
+        }
+
+        protected double earnings() {
+            return 0;
+        }
+    }
+
+    public static void main(String []args) {
+        BasePlusCommissionEmployee comm1 = new BasePlusCommissionEmployee("Carlos", "Souza", "0119",
+                20456.75, 9.5, 15006.00);
+        var s = comm1.toString();
+        System.out.println("your result: " + s);
+    } //o resultado da base salario está oculto por algum motivo.
 }
--- src/ListaCap9/Cap9Q4.java
+++ src/ListaCap9/Cap9Q4.java
@@ -8,6 +8,12 @@ public class Cap9Q4 {
      */
 
     public static void main(String []args) {
-
+        /* O conceito de heranca tem como utilidades:
+         - 1: Evitar a reutilização constante de codigo na aplicacao;
+         - 2: Evitar maior consumo de processamento do hardware;
+         - 3: Gerar maior flexibilidade e agilidade para os desenvolvedores;
+         - 4: Reduz a quantidade de alteracoes a serem feitas no codigo, e com
+         isso diminui a porcentagem de erros cometidos na codificacao;
+        */
     }
 }
